a76fc1fcc7eeb9f1049f2097915f401798da98ff
WILLIAMNEIRA <wamonroyn@ufpso.edu.co>
2022-11-26 16:50:04 -0500

    mapeo de otros elementos

--- src/main/java/com/co/qvision/userinterfaces/HomePage.java
+++ src/main/java/com/co/qvision/userinterfaces/HomePage.java
@@ -9,12 +9,12 @@ public class HomePage {
     public static final Target LBL_DISABLE= Target.the("Captura texto").locatedBy("//input[@name='my-disabled']");
     public static final Target LBL_READONLY = Target.the("Leer texto").locatedBy("//input[@name='my-readonly']");
     public static final Target SELECT_DROPDOWN = Target.the("Seleccionar caja ").locatedBy("//*[@name='my-select']");
-    public static final Target DATALIST_DROPDOWN = Target.the("Seleccionar Lista ").locatedBy("//input[@name='my-datalist']");
+    public static final Target DATALIST_DROPDOWN = Target.the("Seleccionar Lista ").locatedBy("(//input[@name='my-datalist'])");
     public static final Target CHECKED_CHECKBOX = Target.the("Seleccionar Checkbox ").locatedBy("//input[@id='my-check-1']");
     public static final Target DEFAULT_CHECKBOX = Target.the("Seleccionar Checkbox ").locatedBy("//input[@id='my-check-2']");
-    public static final Target CHECKED_RADIO = Target.the("Seleccionar Checkbox ").locatedBy("//input[@id='my-radio-1']");
-    public static final Target DEFAULT_RADIO = Target.the("Seleccionar Checkbox ").locatedBy("//input[@id='my-radio-2']");
-    public static final Target COLOR_PICKER = Target.the("Seleccionar Checkbox ").locatedBy("//input[@name='my-colors']");
+    public static final Target CHECKED_RADIO = Target.the("Seleccionar Checkbox radio ").locatedBy("//input[@id='my-radio-1']");
+    public static final Target DEFAULT_RADIO = Target.the("Seleccionar Checkbox radio por default ").locatedBy("//input[@id='my-radio-2']");
+    public static final Target COLOR_PICKER = Target.the("Seleccionar color ").locatedBy("//input[@name='my-colors']");
 
 
 
